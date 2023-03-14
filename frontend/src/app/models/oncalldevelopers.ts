4c16bd1bf6eb63850389ba13a033d1ffdb0b8fe3
dil98 <dilip.dhital20@gmail.com>
2023-03-14 12:30:21 -0400

    tuesday

--- frontend/src/app/models/oncalldevelopers.ts
+++
@@ -1,5 +0,0 @@
-import { HttpClient } from "@angular/common/http";
-import { Component} from '@angular/core';
-import {observable} from 'rxjs';
-import { onCallDeveloperResponseModel } from 'src/app/models/oncalldevelopers';
-
